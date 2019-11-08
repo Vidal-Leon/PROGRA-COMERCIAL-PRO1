26da815d5b868a3643e5b323efdc3697c0759ef8
Vidal-Leon <vidaldleonlopez164@mesoamericana.edu.gt>
2019-11-08 20:26:29 +0100

    Se crearon los modelos se realizaron las migraciones

--- auto/models.py
+++ auto/models.py
@@ -2,3 +2,34 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
+
+class Carrera(models.Model):
+    lugar = models.CharField(max_length = 90)
+    fechainicio = models.DateField()
+    fechafinal = models.DateField()
+    def _str_(self):
+        return self.lugar
+
+class Piloto(models.Model):
+    nombre = models.CharField(max_length = 70)
+    equipo = models.CharField(max_length = 90)
+    vehiculo = models.CharField(max_length = 200)
+    def _str_(self):
+        return self.nombre
+
+class RegistroCarrera(models.Model):
+    piloto = models.ForeignKey(Piloto, on_delete=models.CASCADE)
+    Posicion = models.CharField(max_length = 70)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    def _str_(self):
+        return self.Posicion
+
+class RegistroCarreraInLine(admin.TabularInline):
+    model = RegistroCarrera
+    extra = 1
+
+class CarreraAdmin(admin.ModelAdmin):
+    inLines = (RegistroCarreraInLine,)
+
+class PilotoAdmin(admin.ModelAdmin):
+    inLines = (RegistroCarreraInLine,)
