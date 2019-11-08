530657e1bd55248dfb0579823377cfd788f7ff9a
Vidal-Leon <vidaldleonlopez164@mesoamericana.edu.gt>
2019-11-08 20:46:24 +0100

    Se realizo una modificacion a el archivo models y se crearon las credenciales para el administrador

--- auto/admin.py
+++ auto/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Carrera, Piloto, RegistroCarrera
 # Register your models here.
+
+admin.site.register(Carrera)
+admin.site.register(Piloto)
+admin.site.register(RegistroCarrera)
--- auto/models.py
+++ auto/models.py
@@ -7,21 +7,21 @@ class Carrera(models.Model):
     lugar = models.CharField(max_length = 90)
     fechainicio = models.DateField()
     fechafinal = models.DateField()
-    def _str_(self):
+    def __str__(self):
         return self.lugar
 
 class Piloto(models.Model):
     nombre = models.CharField(max_length = 70)
     equipo = models.CharField(max_length = 90)
     vehiculo = models.CharField(max_length = 200)
-    def _str_(self):
+    def __str__(self):
         return self.nombre
 
 class RegistroCarrera(models.Model):
     piloto = models.ForeignKey(Piloto, on_delete=models.CASCADE)
     Posicion = models.CharField(max_length = 70)
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
-    def _str_(self):
+    def __str__(self):
         return self.Posicion
 
 class RegistroCarreraInLine(admin.TabularInline):
