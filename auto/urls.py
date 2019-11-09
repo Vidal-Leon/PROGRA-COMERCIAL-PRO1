499a9154abdcc7c9d765d56e7ae795f5f8394e77
Vidal-Leon <vidaldleonlopez164@mesoamericana.edu.gt>
2019-11-09 10:13:02 +0100

    Se inicio el disenio del crud

--- auto/urls.py
+++ auto/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    url(r'^carrera/', views.ListaCarrera, name='ListaCarrera'),
+    url(r'^piloto/', views.ListaPiloto, name='ListaPiloto'),
+    url(r'^registro/', views.ListaRegistro, name='ListaRegistro'),
 ]
--- auto/views.py
+++ auto/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from auto.models import Carrera, Piloto, RegistroCarrera
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def ListaCarrera(request):
+    carrera = Carrera.objects.all
+    return render(request,'carrera/listaCarrera.html',{'carrera':carrera})
+
+def ListaPiloto(request):
+    piloto = Piloto.objects.all
+    return render(request,'piloto/listaPiloto.html',{'piloto':piloto})
+
+def ListaRegistro(request):
+    registro = RegistroCarrera.objects.all
+    return render(request, 'registro/listaRegistro.html',{'registro':registro})
